07c754f47b25792ec02787313638d520dd6ae2c2
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-07-12 22:04:53 -0500

    server running with query params

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hello Gul and Lakota!')
+const express = require('express');
+
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.ENV_PORT || 8080;
+
+// ENDPOINT
+app.get('/', (req, res) => {
+    res.send('Hello TEAM UN!');
+});
+
+// ENDPOINT with query params
+app.get('/api', (req, res) => {
+    const { name, title } = req.query;
+    res.send(`Hello ${title} ${name}!`);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
